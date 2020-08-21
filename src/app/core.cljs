553cd42ff8141e1b52b70581e21b0b1ab9dbfde8
ventrosky <ventrosky@develer.com>
2020-08-21 23:24:31 +0200

    initialize-db

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -1,5 +1,7 @@
 (ns app.core
   (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            [app.db]
             [app.theme :refer [cheffy-theme]]
             ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
 
@@ -12,6 +14,7 @@
 
 (defn ^:dev/after-load start
   []
+  (rf/dispatch-sync [:initialize-db])
   (r/render [app]
     (.getElementById js/document "app")))
 
